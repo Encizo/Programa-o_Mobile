0a69822944044c323254aa9f52be3c6a994c0812
Encizo <smurfencizo@gmail.com>
2024-11-26 21:05:34 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/com/example/arrumar_atividade/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.arrumar_atividade
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/atvdad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atvdad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
